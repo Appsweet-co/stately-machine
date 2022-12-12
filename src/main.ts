ca36d82b9463646c7adb1a3a5d36d7dcce4072fe
dperuo <derek@appsweet.co>
2022-12-12 12:09:59 -0600

    feat: (main.ts) Update API and fix validation

--- src/main.ts
+++ src/main.ts
@@ -1,75 +1,70 @@
+import type { Result } from '@appsweet-co/ts-utils';
 import { filter, Subject } from 'rxjs';
-import type { StatelyError, StatelyTransition } from './const';
+import type { StatelyError, StatelyErrorType, StatelyTransition } from './const';
 
 export class StatelyMachine<T> {
-    #current: T;
-    #transitions: ReadonlyArray<StatelyTransition<T>>;
+  #current: T;
+  #transitions: ReadonlyArray<StatelyTransition<T>>;
 
-    #didChange$$ = new Subject<T>();
-    #didError$$ = new Subject<StatelyError<T>>();
+  #didChange$$ = new Subject<T>();
+  #didError$$ = new Subject<StatelyError<T>>();
 
-    constructor(initial: T) {
-      this.#current = initial;
-      this.#transitions = [];
-    }
-
-    public get state() {
-      return this.#current;
-    }
-
-    public get onAnyError$() {
-      return this.#didError$$.asObservable();
-    }
-
-    public get onAny$() {
-      return this.#didChange$$.asObservable();
-    }
-
-    public on$(state: T) {
-      return this.#didChange$$
-        .asObservable()
-        .pipe(filter(next => next === state));
-    }
-
-    public onError$(type: StatelyError<T>['type']) {
-        return this.#didError$$
-          .asObservable()
-          .pipe(filter(next => next.type === type));
-      }
-
-    public transitions(config: ReadonlyArray<StatelyTransition<T>>) {
-      this.#transitions = config;
-    }
-
-    public go(state: T) {
-      const { error, next } = this.#validate(state);
-
-      if (error)
-        return this.#didError$$.next({
-          type: error,
-          from: this.#current,
-          to: state,
-        });
-
-      this.#current = next;
-      this.#didChange$$.next(next);
-    }
-
-    #validate(state: T) {
-      if (this.#current === state) {
-        return { error: 'SAME_STATE' } as const;
-      }
-
-      if (this.#noTransition(state)) {
-        return { error: 'NO_TRANSITION' } as const;
-      }
-
-      return { next: state } as const;
-    }
-
-    #noTransition(state: T) {
-      return !this.#transitions
-        .filter((item) => item.from.some((item) => item === this.#current))
-        .some((item) => item.to.indexOf(state));
-    }
+  constructor(initial: T) {
+    this.#current = initial;
+    this.#transitions = [];
   }
+
+  public get state() {
+    return this.#current;
+  }
+
+  public get onAny$() {
+    return this.#didChange$$.asObservable();
+  }
+
+  public get onAnyError$() {
+    return this.#didError$$.asObservable();
+  }
+
+  public on$(state: T) {
+    return this.#didChange$$
+      .asObservable()
+      .pipe(filter(next => next === state));
+  }
+
+  public onError$(type: StatelyErrorType) {
+    return this.#didError$$
+      .asObservable()
+      .pipe(filter(next => next.type === type));
+  }
+
+  public transitions(config: ReadonlyArray<StatelyTransition<T>>) {
+    this.#transitions = config;
+  }
+
+  public go(state: T) {
+    const { error, ok } = this.#validate(state);
+
+    if (error) {
+      return this.#didError$$.next({ type: error, from: this.#current, to: state });
+    }
+
+    this.#current = ok;
+
+    return this.#didChange$$.next(ok);
+  }
+
+  #validate(state: T): Result<T, StatelyErrorType> {
+    if (this.#current === state) return { error: 'SAME_STATE' };
+
+    if (this.#noTransition(state)) return { error: 'NO_TRANSITION' };
+
+    return { ok: state };
+  }
+
+  #noTransition(state: T) {
+    return !this.#transitions
+      .filter(item => item.from.includes(this.#current))
+      .some(item => item.to.includes(state));
+  }
+}
